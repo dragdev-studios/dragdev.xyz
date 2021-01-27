b3650de9156179fe08e26b865a9dee1e1bed223c
eek#7574 <eek@clicksminuteper.net>
2021-01-27 12:46:21 +0000

    init commit

---
+++ html/README.md
@@ -0,0 +1,4 @@
+# [beta.dragdev.xyz](https://beta.dragdev.xyz/)
+This is the new website design for DragDev Studios.
+
+[Edit it on github!](https://github.com/EEKIM10/dds.xyz/)
---
+++ main.py
@@ -0,0 +1,19 @@
+import fastapi
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+app = fastapi.FastAPI()
+app.mount("/", StaticFiles(directory="./html", html=True), name="root")
+from starlette.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware, allow_origins=["https://dragdev.xyz", "https://yourapps.cyou"]
+)
+
+
+@app.get("/README.md")
+def readme():
+    return fastapi.responses.RedirectResponse("")
+
+
+uvicorn.run(app, host="0.0.0.0", port=2389)
